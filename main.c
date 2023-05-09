70a2eff75035a8257435583d91f0540594c917bc
Felix Rosén <rosenfelix@gmail.com>
2023-05-09 20:42:18 +0200

    1.3 Aggregate and percentage marks

---
+++ main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  1.3 Aggregate and percentage marks
+//
+//  Created by Felix Rosén on 2023-05-09.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int s1, s2, s3, s4, s5, aggr;
+    
+    float per;
+    
+    printf("Enter the marks of 5 subjects:");
+    
+    scanf("%d %d %d %d %d", &s1, &s2, &s3, &s4, &s5);
+    
+    /* Formula for aggregate and percentage marks */
+          
+    aggr = s1 + s2 + s3 + s4 + s5;
+    
+    per = aggr / 5;
+    
+    printf("Aggregate marks = %d\n", aggr);
+    
+    printf("Aggregate percentage = %f\n", per);
+    
+    return 0;
+}
