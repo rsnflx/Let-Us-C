745bfa92c3f6901ecd53476be71ccf5f94f21e87
Felix Rosén <rosenfelix@gmail.com>
2023-05-09 19:51:31 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  Calculation of Simple Interest
+//
+//  Created by Felix Rosén on 2023-05-09.
+//
+
+/* Calculation of Simple Interest */
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int p, n;
+    
+    float r, si;
+    
+    printf("Enter values of p, n, r:");
+    
+    scanf("%d %d %f", &p, &n, &r);
+    
+    /*formula for simple interest */
+    
+    si = p * n * r / 100;
+    
+    printf("%f\n", si);
+    
+    return 0;
+}
